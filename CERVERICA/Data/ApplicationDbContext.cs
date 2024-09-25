831680450e48707a569f7901ccca24981fc99dd2
CarlosIRB <ramirezbelloci@gmail.com>
2024-09-25 15:16:01 -0600

    Declaracion de tablas iniciales para el CRM

--- CERVERICA/Data/ApplicationDbContext.cs
+++ CERVERICA/Data/ApplicationDbContext.cs
@@ -22,9 +22,13 @@ namespace CERVERICA.Data
         public DbSet<Log> Logs { get; set; }
         public DbSet<PasosReceta> PasosRecetas { get; set; }
         public DbSet<ProduccionLoteInsumo> ProduccionLoteInsumos { get; set; }
-        public DbSet<FavoritoUsuario> FavoritosUsuarios { get; set; }
+        public DbSet<FavoritosComprador> FavoritosComprador { get; set; }
         public DbSet<ProductoCarrito> ProductosCarrito { get; set; }
         public DbSet<Notificacion> Notificaciones { get; set; }
+
+        public DbSet<Comprador> Compradores { get; set; }
+        public DbSet<InteraccionComprador> InteraccionesCompradores { get; set; }
+        public DbSet<PreferenciasComprador> PreferenciasCompradores { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Definición de tablas intermedias de muchos a muchos
@@ -84,6 +88,19 @@ namespace CERVERICA.Data
             .WithMany(i => i.IngredientesReceta)
             .HasForeignKey(o => o.IdInsumo)
             .OnDelete(DeleteBehavior.Restrict);
+
+
+            modelBuilder.Entity<InteraccionComprador>()
+                .HasOne(i => i.Usuario)
+                .WithMany()
+                .HasForeignKey(i => i.IdUsuario)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<PreferenciasComprador>()
+                .HasOne(p => p.Usuario)
+                .WithMany()
+                .HasForeignKey(p => p.IdUsuario)
+                .OnDelete(DeleteBehavior.Cascade);
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ CERVERICA/Models/PreferenciasComprador.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CERVERICA.Models
+{
+    public class PreferenciasComprador
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPreferencia { get; set; }
+
+        [Required]
+        public string IdUsuario { get; set; }  // Relación con ApplicationUser
+        public ApplicationUser Usuario { get; set; }
+
+        public bool RecibePromociones { get; set; }
+        public string EstiloCervezaFavorito { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
