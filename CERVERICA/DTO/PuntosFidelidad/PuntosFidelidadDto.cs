ae564ac98517393fd5cd3ca0b2b6bc5f7f41c129
Erick Rivera <chagoyarc08@gmail.com>
2024-10-29 09:33:23 -0600

    Puntos de fidelida DTO's y controller

---
+++ CERVERICA/DTO/PuntosFidelidad/PuntosFidelidadDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CERVERICA.DTO.PuntosFidelidad
+{
+    public class PuntosFidelidadDto
+    {
+        public int Id { get; set; }
+        public string IdUsuario { get; set; }
+        public int PuntosAcumulados { get; set; }
+        public int PuntosRedimidos { get; set; }
+        public int PuntosDisponibles { get; set; }
+        public DateTime? FechaUltimaActualizacion { get; set; }
+    }
+
+}
