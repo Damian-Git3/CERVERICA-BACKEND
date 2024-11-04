b1138f1b936f89b6087634a6c3f8ee17674a9048
Fernando Gamboa <21002103@alumnos.utleon.edu.mx>
2024-11-04 00:07:38 -0600

    Servicio Whatsapp
    Generacion del servicio para enviar mensaje por whatsapp

---
+++ CERVERICA/Controllers/WhatsAppController.cs
@@ -0,0 +1,25 @@
+﻿using CERVERICA.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CERVERICA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhatsAppController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult<object>> SendWhatsAppMessage(string message, string number)
+        {
+            try
+            {
+
+                await WhatsApp.SendWhatsAppMessage(message, number);
+                return Ok(new { message = "Mensaje enviado correctamente" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Ocurrió un error al enviar el mensaje de WhatsApp.", details = ex.Message });
+            }
+        }
+    }
+}
