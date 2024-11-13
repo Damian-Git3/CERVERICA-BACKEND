e354dcad96ac0b84b31439bbaa860597fdbb97bc
Fernando Gamboa <21002103@alumnos.utleon.edu.mx>
2024-11-13 00:20:27 -0600

    Refactor: Servicios Historial de Precios

--- CERVERICA/DTO/HistorialPrecios/PreciosRecetaDTO.cs
+++ CERVERICA/DTO/HistorialPrecios/PreciosRecetaDTO.cs
@@ -6,10 +6,11 @@
         public string Nombre { get; set; }
         public string Imagen { get; set; }
         public float PrecioLitro { get; set; }
-        public float PrecioPaquet1 { get; set; }
+        public float PrecioPaquete1 { get; set; }
         public float PrecioPaquete6 { get; set; }
         public float PrecioPaquete12 { get; set; }
         public float PrecioPaquete24 { get; set; }
+        public float PrecioUnitarioBaseMayoreo { get; set; }
         public bool Estatus { get; set; }
 
     }
