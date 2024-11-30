65bd3770a35b4374e2b3005adbeff27d06abb9a6
IDGS-802-21000151 <jesusluviancontacto@gmail.com>
2024-11-29 19:16:00 -0600

    Refactor Cupon and EstadisticaComprador models to replace CategoriaComprador with MontoMinimo; add authorization to VentasController; update image URLs in ApplicationDbContext seed data.

--- CERVERICA/Controllers/VentasController.cs
+++ CERVERICA/Controllers/VentasController.cs
@@ -14,6 +14,7 @@ using System.Security.Claims;
 
 namespace CERVERICA.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class VentasController : ControllerBase
