f83cdf0877c6cbc2026c2e87f252430d0be5e175
Erick Rivera <chagoyarc08@gmail.com>
2024-12-03 22:44:24 -0600

    endpoints de asignar pistopoints

---
+++ CERVERICA/DTO/PuntosFidelidad/RegistrarPuntosFidelidad.cs
@@ -0,0 +1,8 @@
+﻿namespace CERVERICA.DTO.PuntosFidelidad
+{
+    public class RegistrarPuntosFidelidad
+    {
+        public decimal MontoCompra { get; set; }
+        public DateTime? FechaUltimaActualizacion { get; set; }
+    }
+}
